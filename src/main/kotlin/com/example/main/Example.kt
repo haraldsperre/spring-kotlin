410f2d1fd74b6b5a1d12572c8b7e8639737381b6
haraldsperre <harald.sperre@gjensidige.no>
2025-03-30 19:50:33 +0200

    Example 14: Singleton Bean Scope inside Spring

--- src/main/kotlin/com/example/main/Example.kt
+++ src/main/kotlin/com/example/main/Example.kt
@@ -1,16 +1,20 @@
 package com.example.main
 
-import com.example.beans.Person
 import com.example.config.ProjectConfig
+import com.example.services.VehicleServices
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 fun main() {
 
     val context = AnnotationConfigApplicationContext(ProjectConfig::class.java)
-    val person = context.getBean(Person::class.java)
 
-    println("Person name from Spring Context is: ${person.getName()}")
-    println("Vehicle that Person owns is: ${person.getVehicle()}")
-    person.getVehicle().getVehicleService().playMusic()
-    person.getVehicle().getVehicleService().moveVehicle()
+    val vehicleService1 = context.getBean(VehicleServices::class.java)
+    val vehicleService2 = context.getBean("vehicleServices", VehicleServices::class.java)
+
+    println("Hashcode of the object vehicleServices1: ${vehicleService1.hashCode()}")
+    println("Hashcode of the object vehicleServices2: ${vehicleService2.hashCode()}")
+
+    if (vehicleService1 == vehicleService2) {
+        println("VehicleServices bean is a singleton scope bean")
+    }
 }
--- src/main/kotlin/com/example/services/VehicleServices.kt
+++ src/main/kotlin/com/example/services/VehicleServices.kt
@@ -2,9 +2,13 @@ package com.example.services
 
 import com.example.interfaces.Speakers
 import com.example.interfaces.Tyres
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
 @Component
+// Singleton is default, but can be included for clarity
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 class VehicleServices(
     val speakers: Speakers,
     var tyres: Tyres
